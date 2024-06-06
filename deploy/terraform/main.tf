8d8e18500669dadeb2c9d45923caf748826bfad4
rniem379349 <robert.niemiec95@wp.pl>
2024-06-06 18:57:24 +0200

    Configure S3 bucket for tfstate storage

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket                  = "weatherapp-tfstate"
+    key                     = "weatherapp"
+    region                  = "us-east-1"
+  }
 
   required_version = ">= 1.8.0"
 }
